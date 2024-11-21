763e902bb6fa92a30f90d5c524348b4a2caa21ee
alefuentes <fuentes_841@hotmail.com>
2024-11-21 20:41:16 -0300

    primer version

---
+++ script.js
@@ -0,0 +1,13 @@
+const lightbox = document.getElementById('lightbox');
+const lightboxImage = document.getElementById('lightboxImage');
+
+function openLightbox(index) {
+    const images = document.querySelectorAll('.thumbnail');
+    lightboxImage.src = images[index].src;
+    lightbox.style.display = 'flex';
+}
+
+function closeLightbox() {
+    lightbox.style.display = 'none';
+    lightboxImage.src = '';
+}
